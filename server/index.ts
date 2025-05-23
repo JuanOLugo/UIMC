7f0446db2f0ecfa66f868e1129c6fbaad7c49669
Juan <juanandresojeda77@gmail.com>
2025-05-22 20:45:48 -0500

    server 4

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
-import express from "express"
-import cors from "cors"
-import CommandRouter from "./Routes/CommandsRoute"
-const app = express()
-app.use(cors())
+import express from "express";
+import cors from "cors";
+import CommandRouter from "./Routes/CommandsRoute";
+import path from "path";
+const app = express();
 
-app.use("/commands", CommandRouter)
+const dirPublic = path.join(__dirname, "public", "Frontend", "browser")
+app.use(express.static(dirPublic));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(dirPublic, "index.html"));
+});
+app.use(cors());
+app.use("/commands", CommandRouter);
+
+
+
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000")
-})
+  console.log("Server running on port 3000");
+});
